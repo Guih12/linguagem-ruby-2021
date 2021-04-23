1127672cab1951a5beeb3cebe3fbe263e5efe409
George Borsato <georginhoborsato@gmail.com>
2021-04-22 21:29:59 -0300

    day week

---
+++ date/day_week.rb
@@ -0,0 +1,14 @@
+def day_of_the_week(y, m, d)
+  t = [ 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 ]
+
+  if m < 3
+    y = y - 1
+  end
+  return (y + y / 4 - y / 100 + y / 400 + t[m-1]+d)%7
+end
+
+day = 13
+month = 7
+year = 2017
+result = day_of_the_week(year, month, day)
+puts "#{result}"
